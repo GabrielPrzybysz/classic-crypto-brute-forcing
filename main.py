6454736bf4a20ef89d5b7470115ba136f490e6dd
Gabriel Przybysz <45472156+GabrielPrzybysz@users.noreply.github.com>
2024-09-30 18:56:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,59 +2,49 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def create_hashset(path):
+    hashset = set()
+
+    with open(path, 'r') as file:
+        for line in file:
+            word = line.strip()
+            hashset.add(word)
+
+    return hashset
+
 hashset = create_hashset('./words.txt')
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return "No file part", 400
-
-    file = request.files['file']
-
-    if file.filename == '':
-        return "No selected file", 400
-
-    if file:
-        file_content = file.read().decode('utf-8')
-        
-        first_word = file_content.split()[0] if file_content else None
-
-        return f"The first word is: {first_word}"
 
 def brute_caesar(word):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
     word = word.lower()
-    
+
     possible_decryptions = []
-    
-    for shift in range(26):
+
+    for shift in range(1, 26):
         decrypted_word = ''
-        
+
         for char in word:
             if char in alphabet:
                 shifted_index = (alphabet.index(char) - shift) % 26
                 decrypted_word += alphabet[shifted_index]
             else:
                 decrypted_word += char
-        
+
         possible_decryptions.append((shift, decrypted_word))
 
-def create_hashset(path):
-    hashset = set()
+    return possible_decryptions
 
-    with open(path, 'r') as file:
-        for line in file:
-            word = linha.strip()
-            hashset.add(palavra)
+def format(possible_decryptions):
+    result = ""
+    for shift, word in possible_decryptions:
+        if word in hashset:
+            result += f"<h3 style=\"color:green\"> Key: {shift}, word: {word} </h3> <br>"
+        else:
+            result += f"<h3> Key: {shift}, word: {word} </h3> <br>"
 
-    return hashset
+    return result
 
-
-
-
-
+@app.route("/")
+def index():
+    result = brute_caesar("cpn")
+    return format(result)
